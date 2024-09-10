8ba6ff5913efeba27571e541b73df1ffc00c70b5
Fitsumhelina <permin1501424@gmail.com>
2024-09-11 02:37:19 +0300

    chat id changed

--- bot.js
+++ bot.js
@@ -67,7 +67,7 @@ bot.on('message', (msg) => {
 
 // Handle the admin's reply to a specific user
 bot.on('callback_query', (callbackQuery) => {
-  const chatId = callbackQuery.message.chat.id;
+  const chatId = adminChatId;
 
   // Only allow the admin to reply
   if (chatId !== adminChatId) {
