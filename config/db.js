3ab572ddb26e6cfa63ce080d80e76a9cb7cb0c89
Fitsumhelina <permin1501424@gmail.com>
2024-09-14 13:34:59 +0300

    database connction improved

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
